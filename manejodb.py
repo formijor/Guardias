ff75aaa204240d1bf74ddb0ba88c807cca6796c0
formijor <formijor@gmail.com>
2018-08-07 04:22:25 -0300

    Reorganizado repo

---
+++ manejodb.py
@@ -0,0 +1,15 @@
+'''
+Created on 17 jul. 2018
+
+@author: formijor
+'''
+
+
+def obtener_vacaciones(persona):
+    vacaciones = ''
+    sql = '''SELECT * FROM      
+            WHERE persona = %d''', persona
+    #codigo obtiene directo de la bd
+    return vacaciones
+
+def obtener_
